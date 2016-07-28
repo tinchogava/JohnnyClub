28fd7f9f63dd15499e61b6c1f9468e462497f4fb
root <root@ip-172-31-49-184.ec2.internal>
2016-07-28 15:49:16 +0000

    commit puesta a punto

---
+++ app/Http/Requests/SaveProductRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class SaveProductRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:categories|max:255',
+            'description' => 'required|max:400',
+            'size' => 'required',
+            'price' => 'required',
+            'category_id' => 'required',
+            'varietal_id' => 'required',
+            'winery_id' => 'required',
+
+        ];
+    }
+}
+
